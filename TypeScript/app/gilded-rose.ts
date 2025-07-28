bbd015c4d6dfd938cf5a121029a0f2a8fe918b14
Rutger Walma <r.walma@tfe.nl>
2025-07-28 21:23:45 +0200

    feat: Refactored gilded-rose.ts, wrote some tests

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -18,52 +18,41 @@ export class GildedRose {
   }
 
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-        }
-      }
+    function updateAgedBrie(item: Item) {
+      if (item.quality < 50) item.quality++;
     }
 
+    function updateBackstagePass(item: Item) {
+      if (item.sellIn <= 0) item.quality = 0;
+      else if (item.sellIn <= 5) item.quality += 3;
+      else if (item.sellIn <= 10) item.quality += 2;
+      else item.quality++;
+    }
+
+    function updateDefault(item: Item) {
+      item.quality -= item.sellIn <= 0 ? 2 : 1;
+      if (item.quality < 0) item.quality = 0;
+    }
+
+    function updateConjured(item: Item) {
+      item.sellIn < 0 ? (item.quality -= 4) : (item.quality -= 2);
+    }
+
+    function clampQuality(item: Item) {
+      if (item.quality > 50) item.quality = 50;
+      if (item.quality < 0) item.quality = 0;
+    }
+
+    this.items.forEach((item) => {
+      if (item.name === "Sulfuras, Hand of Ragnaros") return;
+      if (item.name === "Aged Brie") updateAgedBrie(item);
+      else if (item.name.includes("Backstage pass")) updateBackstagePass(item);
+      else if (item.name.includes("Conjured")) updateConjured(item);
+      else updateDefault(item);
+
+      clampQuality(item);
+      item.sellIn--;
+    });
     return this.items;
   }
 }
--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,23 @@
-import { Item, GildedRose } from '@/gilded-rose';
+import { Item, GildedRose } from "@/gilded-rose";
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+describe("Backstage pass", () => {
+  it("should expire", () => {
+    const backstagePass = new Item(
+      "Backstage passes to a TAFKAL80ETC concert",
+      0,
+      50
+    );
+    const gildedRose = new GildedRose([backstagePass]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    expect(items[0].quality).toBe(0);
+  });
+});
+
+describe("Conjured item", () => {
+  it("should degrade twice as fast", () => {
+    const conjuredItem = new Item("Conjured Eierbal", 4, 10);
+    const gildedRose = new GildedRose([conjuredItem]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(8);
   });
 });
